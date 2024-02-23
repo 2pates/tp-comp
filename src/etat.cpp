6f4ac3ba347dd9d0415e4871b22ed83594e8a584
Gaspard3.6 <gaspard.serpinet@insa-lyon.fr>
2024-02-23 10:23:27 +0100

    feature etat

--- src/etat.cpp
+++ src/etat.cpp
@@ -9,9 +9,14 @@ void decalage(Analyseur& a, Etat* e, Symbole* s) {
 	a.lexer.Avancer();
 }
 
-void reduction(Analyseur& a, Etat* e, Symbole* s, int nbReducedSymbol)
+void reduction(Analyseur& a, Symbole* s, int nbReducedSymbol)
 {
-	
+	for(int i=0; i < nbReducedSymbol; i++)
+	{
+		a.automate.pile_etats.pop_back();
+		a.automate.pile_symboles.pop_back();
+	}
+	a.automate.pile_symboles.push_back(s);
 }
 
 
@@ -43,14 +48,67 @@ bool I0::transition(Analyseur& a, Symbole* s) {
 }
 
 
-bool I1::transition(Analyseur& automate, Symbole* s) {
+bool I1::transition(Analyseur& a, Symbole* s) {
+	switch ((int)s) {
+	case PLUS:
+		decalage(a, new I4(), s);
+		break;
+
+	case MULT:
+		decalage(a, new I5(), s);
+		break;
+	
+	case FIN:
+		// idk ?
+		break;
+
+	default:
+		return false;
+		break;
+	}
+
+	return true;
+}
+
+
+bool I2::transition(Analyseur& a, Symbole* s) {
 	switch ((int)s) {
 	case INT:
 		decalage(a, new I3(), s);
 		break;
 
 	case OPENPAR:
-		decalage(a, new I3(), s);
+		decalage(a, new I2(), s);
+		break;
+	
+	case E:
+		transition_simple(a, new I6(), s);
+	
+	default:
+		return false;
+		break;
+	}
+
+	return true;
+}
+
+
+bool I3::transition(Analyseur& a, Symbole* s) {
+	switch ((int)s) {
+	case PLUS:
+		reduction(a, s, 5);
+		break;
+
+	case MULT:
+		reduction(a, s, 5);
+		break;
+	
+	case CLOSEPAR:
+		reduction(a, s, 5);
+		break;
+	
+	case FIN:
+		reduction(a, s, 5);
 		break;
 	
 	default:
@@ -62,36 +120,40 @@ bool I1::transition(Analyseur& automate, Symbole* s) {
 }
 
 
-bool I0::transition(Analyseur& automate, Symbole* s) {
+bool I4::transition(Analyseur& a, Symbole* s) {
 	switch ((int)s) {
 	case INT:
 		decalage(a, new I3(), s);
 		break;
 
 	case OPENPAR:
+		decalage(a, new I2(), s);
+		break;
+	
+	case E:
+		transition_simple(a, new I7(), s);
+	
+	default:
+		return false;
+		break;
+	}
+
+	return true;
+}
+
+
+bool I5::transition(Analyseur& a, Symbole* s) {
+	switch ((int)s) {
+	case INT:
 		decalage(a, new I3(), s);
 		break;
-	
-	default:
-		return false;
-		break;
-	}
-
-	return true;
-}
-
-
-bool I0::transition(Analyseur& automate, Symbole* s) {
-	switch ((int)s) {
-	case INT:
-		automate.pile_etats.push_back(new I3());
-		automate.pile_symboles.push_back(s);
-		break;
 
 	case OPENPAR:
-		automate.pile_etats.push_back(new I2());
-		automate.pile_symboles.push_back(s);
+		decalage(a, new I2(), s);
 		break;
+	
+	case E:
+		transition_simple(a, new I8(), s);
 	
 	default:
 		return false;
@@ -102,18 +164,20 @@ bool I0::transition(Analyseur& automate, Symbole* s) {
 }
 
 
-bool I0::transition(Analyseur& automate, Symbole* s) {
+bool I6::transition(Analyseur& a, Symbole* s) {
 	switch ((int)s) {
-	case INT:
-		automate.pile_etats.push_back(new I3());
-		automate.pile_symboles.push_back(s);
+	case PLUS:
+		decalage(a, new I4(), s);
 		break;
 
-	case OPENPAR:
-		automate.pile_etats.push_back(new I2());
-		automate.pile_symboles.push_back(s);
+	case MULT:
+		decalage(a, new I5(), s);
 		break;
 	
+	case CLOSEPAR:
+		decalage(a, new I9(), s);
+		break;
+
 	default:
 		return false;
 		break;
@@ -123,18 +187,24 @@ bool I0::transition(Analyseur& automate, Symbole* s) {
 }
 
 
-bool I0::transition(Analyseur& automate, Symbole* s) {
+bool I7::transition(Analyseur& a, Symbole* s) {
 	switch ((int)s) {
-	case INT:
-		automate.pile_etats.push_back(new I3());
-		automate.pile_symboles.push_back(s);
+	case PLUS:
+		reduction(a, s, 2);
 		break;
 
-	case OPENPAR:
-		automate.pile_etats.push_back(new I2());
-		automate.pile_symboles.push_back(s);
+	case MULT:
+		decalage(a, new I5(), s);
 		break;
 	
+	case CLOSEPAR:
+		reduction(a, s, 2);
+		break;
+
+	case FIN:
+		reduction(a, s, 2);
+		break;
+
 	default:
 		return false;
 		break;
@@ -143,5 +213,49 @@ bool I0::transition(Analyseur& automate, Symbole* s) {
 	return true;
 }
 
+
+bool I8::transition(Analyseur& a, Symbole* s) {
+	switch ((int)s) {
+	case PLUS:
+		reduction(a, new Symbole(E), 3);
+		break;
+	case MULT:
+		reduction(a, new Symbole(E), 3);
+		break;
+	case CLOSEPAR:
+		reduction(a, new Symbole(E), 3);
+		break;
+	case FIN:
+		reduction(a, new Symbole(E), 3);
+		break;
+	default:
+		return false;
+		break;
+	}
+
+	return true;
+}
+
+bool I9::transition(Analyseur& a, Symbole* s) {
+	switch ((int)s) {
+	case PLUS:
+		reduction(a, new Symbole(E), 3);
+		break;
+	case MULT:
+		reduction(a, new Symbole(E), 3);
+		break;
+	case CLOSEPAR:
+		reduction(a, new Symbole(E), 3);
+		break;
+	case FIN:
+		reduction(a, new Symbole(E), 3);
+		break;
+	default:
+		return false;
+		break;
+	}
+
+	return true;
+}
 
 
