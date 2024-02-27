0e3755112b4b9ee89fd561f32ec00706ead17caf
Benjamin Maison <benjamin.maison@insa-lyon.fr>
2024-02-27 14:40:28 +0100

    implement destructor for automate

--- src/automate.cpp
+++ src/automate.cpp
@@ -5,6 +5,13 @@ Automate::Automate() {
 	pile_etats.push_back(new I0());
 }
 
-Automate::~Automate() {}
-
-
+Automate::~Automate() {
+	for (auto it = pile_etats.begin(); it != pile_etats.end(); it++)
+	{
+		delete *it;
+	}
+	for (auto it = pile_symboles.begin(); it != pile_symboles.end(); it++)
+	{
+		delete *it;
+	}
+}
